34abb69741c76239ba1a7a9bf31f3d06bac0be24
vissu99 <vissu.nalmas@gmail.com>
2020-10-06 11:17:16 +0100

    service tier

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -4,8 +4,8 @@ set -ex
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
+kubectl delete -f deployment/pages-deployment.yaml
 kubectl apply -f deployment/pages-deployment.yaml
-
 kubectl apply -f deployment/mysql-secret.yaml
 kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
