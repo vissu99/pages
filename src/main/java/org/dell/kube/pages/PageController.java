ed7b446e0418f91d65730578f55e7f10fd7e185f
vissu99 <vissu.nalmas@gmail.com>
2020-10-06 12:57:23 +0100

    Enabling log and monitoring

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -1,5 +1,9 @@
 package org.dell.kube.pages;
 
+
+
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,8 @@ import java.util.List;
 
 public class PageController {
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(PageController.class);
+
     private IPageRepository pageRepository;
 
     public PageController(IPageRepository pageRepository) {
@@ -25,6 +31,8 @@ public class PageController {
     public ResponseEntity<Page> create(@RequestBody Page page) {
 
         Page newPage = pageRepository.create(page);
+        logger.info("CREATE-INFO:Created page with id = "+newPage.getId());
+        logger.debug("CREATE-DEBUG:Created page with id = "+newPage.getId());
 
         return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
 
@@ -34,22 +42,24 @@ public class PageController {
 
     public ResponseEntity<Page> read(@PathVariable long id) {
 
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
         Page page = pageRepository.read(id);
-
-        if (page != null)
-
+        if(page!=null)
             return new ResponseEntity<Page>(page, HttpStatus.OK);
-
-        else
-
+        else {
+            logger.error("READ-ERROR:Could not find page with id = " + id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
-
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<Page>> list() {
 
         List<Page> pages = pageRepository.list();
+        logger.info("READ-INFO:Reading pages list with size = "+pages.size());
+        logger.debug("READ-DEBUG:Reading pages list with size = "+pages.size());
 
         return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
 
@@ -59,10 +69,14 @@ public class PageController {
 
     public ResponseEntity<Page> update(@RequestBody Page page, @PathVariable long id) {
         Page updatedPage = pageRepository.update(page, id);
+        logger.info("UPDATE-INFO:Updating page with id = "+id);
+        logger.debug("UPDATE-DEBUG:Updating page with id = "+id);
         if (updatedPage != null)
             return new ResponseEntity<Page>(updatedPage, HttpStatus.OK);
-        else
+        else {
+            logger.error("UPDATE-ERROR: Coult not find page with id = "+id);
             return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("{id}")
@@ -70,6 +84,8 @@ public class PageController {
     public ResponseEntity delete(@PathVariable long id) {
 
         pageRepository.delete(id);
+        logger.info("DELETE-INFO:Deleting page with id = "+id);
+        logger.debug("DELETE-DEBUG:Deleting page with id = "+id);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
 
