dbd6e894cc0f70565b5b84bee4de0b4f7a53f06c
vissu99 <vissu.nalmas@gmail.com>
2020-10-07 12:30:21 +0100

    Feign & cloud servies enablement

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#Hands-on Labs for Developing a Pages microservice using Spring Boot and deploying to K8 cluster
-
+Spring boot microservice - Category
--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -3,7 +3,9 @@ package org.dell.kube.pages;
 
 
 import ch.qos.logback.classic.Logger;
+import feign.FeignException;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -26,16 +28,37 @@ public class PageController {
         this.pageRepository = pageRepository;
 
     }
+    @Autowired
+    private CategoryClient categoryClient;
 
     @PostMapping
     public ResponseEntity<Page> create(@RequestBody Page page) {
 
-        Page newPage = pageRepository.create(page);
-        logger.info("CREATE-INFO:Created page with id = "+newPage.getId());
-        logger.debug("CREATE-DEBUG:Created page with id = "+newPage.getId());
+        logger.info("CREATE-INFO:Creating a new page");
+        logger.debug("CREATE-DEBUG:Creating a new  page");
+        Category category = null;
+        try {
+            category = categoryClient.findCategory(page.getCategoryId());
+        }
+        catch(FeignException ex){
+            if(ex.getMessage().contains("404")) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
 
-        return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
-
+        if(category ==null || category.getId()==null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else
+        {
+            Page newPage = pageRepository.create(page);
+            logger.info("CREATE-INFO:Created a new page with id = " + newPage.id);
+            logger.debug("CREATE-DEBUG:Created a new  page with id = " + newPage.id);
+            return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+        }
     }
 
     @GetMapping("{id}")
